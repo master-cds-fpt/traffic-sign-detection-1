6750331e1cd5d93bd85f5eb359c48c955b64a2c5
Uday Mittal <uday19@gmail.com>
2016-03-14 18:51:21 -0700

    Test script to read images

--- code/s_readData.m
+++ code/s_readData.m
@@ -3,5 +3,11 @@ imageRoot = '../files/';
 
 %read ground truth file
 gtFilePath = strcat(imageRoot, 'gt.txt');
+
+%rGTdata contains fileNo, ROI, and class info
 rGTdata = TSD_readGTData(gtFilePath);
 
+%example on how to read and show image
+
+img = imread(strcat(imageRoot, '0000.ppm'));
+imshow(img);
