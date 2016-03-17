5da0216bf11d53b4215fc6e3fa51c55d0902d022
Uday Mittal <uday19@gmail.com>
2016-03-17 15:29:51 -0700

    Template matching changes and filtering

--- code/f_template_matching.m
+++ code/f_template_matching.m
@@ -1,11 +1,11 @@
 function [rois] = f_template_matching(img_name,threshold)
-	load(strcat('..\mandatory\',img_name),'img');
+	load(strcat('../mandatory/',img_name),'img');
 	mandatory_img = img;
 
-	load(strcat('..\prohibitory\',img_name),'img');
+	%load(strcat('..\prohibitory\',img_name),'img');
 	prohibitory_img = img;
 
-	load(strcat('..\prohibitory\',img_name),'img');
+	%load(strcat('..\prohibitory\',img_name),'img');
 	danger_img = img;
 
 	% mandatory_img = imread(strcat('..\mandatory\',img_name));
@@ -20,19 +20,30 @@ function [rois] = f_template_matching(img_name,threshold)
 	prohibitory_templates = extract_templates(prohibitory_template_path);
 	danger_templates = extract_templates(danger_template_path);
 
+    
+    %mandatory image, prohibitory image and danger image are svm outputs of
+    %the same image with corresponding svms
+    
 	scale = 1.1;
 	[rois,score] = sub_scale_matching(mandatory_img,prohibitory_img,danger_img, ...
 		  mandatory_templates,prohibitory_templates,danger_templates,threshold);
 	order = zeros(size(score));
 
-	% for k = 1:22
-	% 	img = imresize(img,1/scale);
-	% 	[rois_,score_] = sub_scale_matching(img,template,threshold);
-	% 	rois = [rois floor(rois_./(scale^k))];
-	% 	score = [score score_];
-	% end
+	for k = 1:22
+		mandatory_img = imresize(mandatory_img,1/scale);
+        prohibitory_img = imresize(prohibitory_img,1/scale);
+        danger_img = imresize(danger_img,1/scale);
+        
+		[rois_,score_] = sub_scale_matching(mandatory_img,prohibitory_img,danger_img, ...
+		  mandatory_templates,prohibitory_templates,danger_templates,threshold);
+		rois = [rois ;floor(rois_.*(scale^k))];
+		score = [score ; score_];
+    end
 	
-	% rois = remove_overlapping_regions(rois,score)
+    ids = find((rois(:,1) > 0) & (rois(:,2) > 0));
+    rois = rois(ids,:);
+    score = score(ids);
+	rois = remove_overlapping_regions(rois,score);
 end
 
 function [rois,score] = sub_scale_matching(mandatory_img,prohibitory_img,danger_img, ...
@@ -42,27 +53,24 @@ function [rois,score] = sub_scale_matching(mandatory_img,prohibitory_img,danger_
 	R = zeros(size(mandatory_img));
 	for i =1:size(mandatory_templates,3)
 		R = bsxfun(@max,R,conv2(mandatory_img, mandatory_templates(:,:,i),'SAME'));
-	end
+    end
+    
+    [xdetections, ydetections, score] = get_distinct_detections(R);
+    [rois] = generate_rois_from_points(xdetections, ydetections);
+    
+% 	for i =1:size(prohibitory_templates,3)
+% 		R = bsxfun(@max,R,conv2(prohibitory_img, prohibitory_templates(:,:,i),'SAME'));
+% 	end
+% 
+% 	for i =1:5
+% 		size(danger_templates)
+% 		R = bsxfun(@max,R,1.2*conv2(danger_img, danger_templates(:,:,i),'SAME'));
+% 	end
+% 
+% 	for i =6:10
+% 		R = bsxfun(@max,R,conv2(danger_img, danger_templates(:,:,i),'SAME'));
+% 	end
 
-	for i =1:size(prohibitory_templates,3)
-		R = bsxfun(@max,R,conv2(prohibitory_img, prohibitory_templates(:,:,i),'SAME'));
-	end
-
-	for i =1:5
-		size(danger_templates)
-		R = bsxfun(@max,R,1.2*conv2(danger_img, danger_templates(:,:,i),'SAME'));
-	end
-
-	for i =6:10
-		R = bsxfun(@max,R,conv2(danger_img, danger_templates(:,:,i),'SAME'));
-	end
-
-	threshold = 0.5*max(max(R));
-
-	[y,x] = find(R>threshold);
-	size(x)
-	rois = []
-	score = []
 	% for id =1:size(x,1)
 	% 	roi = [x-7 y-7 x+8 y+8];
 	% 	rois = [rois roi];
@@ -82,25 +90,31 @@ function templates = extract_templates(template_path)
 end
 
 function [resRoi,resScore] = remove_overlapping_regions(roi,score)
-	flag  = ones(size(roi,1));
+	flag  = ones(size(roi,1),1);
 	for i = 1:size(roi,1)
 		if flag(i)==0
 			continue;
 		end
 
 		for j = i+1:size(roi,1)
+            if flag(j)==0
+        		continue;
+            end
+
 			if get_overlap(roi(i,:),roi(j,:)) > 0.2
-				if score(i) > score(j)
+				[i,j]
+                if score(i) > score(j)
 					flag(j)=0;
 				else
 					flag(i)=0;
 				end
 			end
 		end
-	end 
-
-	resRoi = roi(flag(:),:);
-	resScore = score(flag(:));
+    end 
+    size(flag==1)
+    size(roi)
+	resRoi = roi(flag==1,:);
+	resScore = score(flag==1);
 
 end
 
@@ -126,5 +140,60 @@ function overlap = get_overlap( resRoi, gtRoi )
 	    A2 = (gtRoi(3) - gtRoi(1) + 1) * (gtRoi(4) - gtRoi(2) + 1);
 
 	    overlap = max([intersectArea/A1 intersectArea/A2]);
+        overlap
 	end
+end
+
+
+function [xdetections, ydetections, score] = get_distinct_detections(R)
+%R: Response image
+%sort the response from high to low
+    [val, ind] = sort(R(:), 'descend');
+    detcount = 1;
+    itr = 1;
+    xdetections = [];
+    ydetections = [];
+	score = [];
+    %iterate through all values from convolution
+    %keeping best 10 detections
+    while ((detcount <= 3) && (itr < length(ind)))
+        [y, x] = ind2sub([size(R,1), size(R,2)], ind(itr));
+        overlap = false;
+        
+        %check overlap using euclidean distance
+        if detcount > 1
+            X = [xdetections(:), ydetections(:)];
+            Y = [x, y];
+            distances = pdist2(X, Y);
+            minDistance = min(distances(:));
+            if minDistance  <= 100
+                overlap = true;
+            end
+        end
+        
+        %add detection if no overlap
+        if(~overlap)
+            xdetections(detcount) = x;
+            ydetections(detcount) = y;
+            score(detcount) = val(itr);
+            detcount = detcount +1;
+        end
+        itr = itr+1;
+    end
+    
+    display(xdetections);
+    display(ydetections);
+    display(score);
+    
+end
+
+function [rois] = generate_rois_from_points(xdetections, ydetections)
+    rois = zeros([3,4]);
+    w = 16;
+    h = 16;
+    for i = 1:length(xdetections)
+        xmin = xdetections(i) - 7;
+        ymin = ydetections(i) - 7;
+        rois(i, :) =[xmin, ymin ,xdetections(i)+8, ydetections(i)+8];
+    end
 end
